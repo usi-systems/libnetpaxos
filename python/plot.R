e3217fd8baaa5832fc4725251355058ef0ccee13
Tu Dang <huynh.tu.dang@usi.ch>
2016-03-17 13:25:03 +0100

    read latency file and aggregate in a dataframe

--- python/plot.R
+++ python/plot.R
@@ -1,11 +1,43 @@
+#!/usr/local/bin/Rscript
 
 library(ggplot2)
+library(tools)
+
+
+# Input file should be named "device-trial.csv"
+
+readLatency <- function(x) {
+    df <- read.csv(x, col.names=c('latency'))
+    df$latency <- df$latency * 10^6
+    fname <- basename(file_path_sans_ext(x))
+    title <- strsplit(fname, "-")
+    device_name <- title[[1]][1]
+    trial_time <-  title[[1]][2]
+    df$device <- device_name
+    df$trial <- trial_time
+    return(df)
+}
 
 args <- commandArgs(TRUE)
-df <- read.csv(args[1], col.names=c('latency'))
-pdf("cumulative.pdf")
-ggplot(df, aes(latency)) + stat_ecdf(geom = "step")
 
-df$typ = 'normal'
+data <- data.frame(device=character(), trial=character(),latency=numeric())
+for (i in args) {
+    df <- readLatency(i)
+    print(summary(df))
+    data <- rbind(data, df)
+}
+
+
+pdf("cumulative.pdf")
+ggplot(data, aes(latency)) +
+stat_ecdf(geom = "step") +
+ylab("") +
+ggtitle("CDF for latency")
+dev.off()
+
 pdf("boxplot.pdf")
-ggplot(df, aes(factor(typ), latency)) + geom_boxplot()
+ggplot(data, aes(device, latency)) +
+geom_boxplot() +
+ggtitle("boxplot latency")
+
+dev.off()
