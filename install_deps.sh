d1bf8aa8f30337f23f470b8517078533d3dcdd69
Tu Dang <huynh.tu.dang@usi.ch>
2016-04-19 10:18:17 +0200

    script to install leveldb on Debian and Red Hat. Update cmake build

---
+++ install_deps.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+if [ -f /etc/debian_version ]; then
+    OS="Debian"
+    VER=$(cat /etc/debian_version)
+    apt-get install -y libsnappy1 libsnappy-dev
+elif [ -f /etc/redhat-release ]; then
+    OS="Red Hat"
+    VER=$(cat /etc/redhat-release)
+    yum install -y snappy snappy-devel
+fi
+
+cd /tmp && wget https://github.com/google/leveldb/archive/v1.18.tar.gz
+mkdir leveldb && tar -xf v1.18.tar.gz -C leveldb --strip-components 1
+cd leveldb
+make
+cp --preserve=links libleveldb.* /usr/local/lib
+cp -r include/leveldb /usr/local/include/
+ldconfig
