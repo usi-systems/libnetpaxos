67b82b80ea5de2df84e70ef6d73392473f1147a7
Tu Dang <huynh.tu.dang@usi.ch>
2016-04-20 14:04:58 +0200

    Fixed bug in leveldb client

--- app/leveldb_client.c
+++ app/leveldb_client.c
@@ -8,7 +8,10 @@
 char *msg[] = { "PUT key val", "GET key", "DEL key", "GET not"};
 
 struct info {
+    struct event_base *base;
     int mps;
+    int total_commands;
+    char **msg;
 };
 
 void monitor(evutil_socket_t fd, short what, void *arg) {
@@ -40,54 +43,80 @@ void readcb(struct bufferevent *bev, void *ptr)
 void eventcb(struct bufferevent *bev, short events, void *ptr)
 {
     if (events & BEV_EVENT_CONNECTED) {
-         printf("Connect okay.\n");
-         submit(bev, msg[0], 12);
+        printf("Connect okay.\n");
+        bufferevent_enable(bev, EV_READ|EV_WRITE);
+        submit(bev, msg[0], 12);
     } else if (events & (BEV_EVENT_ERROR|BEV_EVENT_EOF)) {
-         struct event_base *base = ptr;
+         struct info *inf = ptr;
          if (events & BEV_EVENT_ERROR) {
                  int err = bufferevent_socket_get_dns_error(bev);
                  if (err)
                          printf("DNS error: %s\n", evutil_gai_strerror(err));
          }
          printf("Closing\n");
-         bufferevent_free(bev);
-         event_base_loopexit(base, NULL);
+         event_base_loopexit(inf->base, NULL);
     }
 }
 
-int main(int argc, char* argv[])
-{
-    if (argc < 3) {
-        printf("Usage: %s address port\n", argv[0]);
+void read_input(struct info *inf, char* workload) {
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    FILE *fp = fopen(workload, "r");
+    if (fp == NULL)
         exit(EXIT_FAILURE);
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+        printf("Retrieved line of length %zu :\n", read);
+        printf("%s", line);
     }
+    fclose(fp);
+}
+
+struct info *info_new() {
     struct info *inf = malloc(sizeof(struct info));
     inf->mps = 0;
+    inf->total_commands = 0;
+}
 
-    struct event_base *base;
+void info_free(struct info *inf) {
+    event_base_free(inf->base);
+    free(inf);
+}
+
+
+int main(int argc, char* argv[])
+{
+    if (argc < 4) {
+        printf("Usage: %s address port workload\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    struct info *inf = info_new();
+
     struct evdns_base *dns_base;
     struct bufferevent *bev;
     struct event *ev_monitor;
-    struct sockaddr_in sin;
 
-    base = event_base_new();
-    dns_base = evdns_base_new(base, 1);
+    inf->base = event_base_new();
+    dns_base = evdns_base_new(inf->base, 1);
 
     int port = atoi(argv[2]);
 
-    bev = bufferevent_socket_new(base, -1, BEV_OPT_CLOSE_ON_FREE);
-
+    bev = bufferevent_socket_new(inf->base, -1, BEV_OPT_CLOSE_ON_FREE);
     bufferevent_setcb(bev, readcb, NULL, eventcb, inf);
-    bufferevent_enable(bev, EV_READ|EV_WRITE);
+
     if (bufferevent_socket_connect_hostname(bev, dns_base, AF_INET, argv[1], port) < 0) {
-        bufferevent_free(bev);
-        return -1;
+        perror("connect failed.\n");
     }
 
-    ev_monitor = event_new(base, -1, EV_TIMEOUT|EV_PERSIST, monitor, inf);
+    ev_monitor = event_new(inf->base, -1, EV_TIMEOUT|EV_PERSIST, monitor, inf);
     struct timeval one_second = {1, 0};
     event_add(ev_monitor, &one_second);
 
-    event_base_dispatch(base);
+    read_input(inf, argv[3]);
+
+    event_base_dispatch(inf->base);
+    bufferevent_free(bev);
+    info_free(inf);
     return 0;
 }
