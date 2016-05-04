dfca061c7e78a058493df7dcec400ceb1be306b3
Tu Dang <huynh.tu.dang@usi.ch>
2016-05-04 23:46:18 +0200

    sample latency for every 2000 packets

--- lib/src/proposer.c
+++ lib/src/proposer.c
@@ -60,7 +60,9 @@ void on_response(evutil_socket_t fd, short what, void *arg) {
             fprintf(stderr, "Latency is negative\n");
         } else {
             double latency = (result.tv_sec + ((double)result.tv_nsec) / 1e9);
-            fprintf(stdout, "%.9f\n", latency);
+            if (*req_id % 2000 == 0) {
+                fprintf(stdout, "%.9f\n", latency);
+            }
         }
         if (state->conf->verbose) {
             printf("on value: %s: %d length, addr_length: %d\n", recvbuf, n, remote_len);
